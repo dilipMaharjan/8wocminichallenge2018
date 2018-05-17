25ef77e406ca5ce76466fb15d667cabfc37cd086
dilip <dilipmaharjann@gmail.com>
2018-05-17 13:55:22 -0400

    error message, hide softkeyad

--- app/src/main/java/com/a8wocminichallenge2018/MainActivity.kt
+++ app/src/main/java/com/a8wocminichallenge2018/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.a8wocminichallenge2018
 
 import android.app.ProgressDialog
+import android.content.Context
+import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
 import android.util.Log
+import android.util.Patterns
+import android.view.MenuItem
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.webkit.URLUtil
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.BufferedInputStream
 import java.io.BufferedReader
@@ -17,6 +29,8 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private var progressDialog: ProgressDialog? = null
+    private var urlEditTv: EditText? = null
+    private var btnView: Button? = null
 
     companion object {
         private const val TAG = "MAIN_ACTIVITY"
@@ -25,14 +39,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_parse.setOnClickListener {
-            Log.i(TAG, "Test")
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        btnView = btn_view
+        urlEditTv = url
+
+        btn_view.setOnClickListener {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(url.windowToken, 0)
+
             //Nepali
             //  GetUsfm(usfm_to_txt).execute("https://cdn.door43.org/ne/ulb/v5.2/mat.usfm")
 
             //Serbian
-            GetUsfm(usfm_to_txt).execute("https://cdn.door43.org/sr-Latn/stf/v1/mat.usfm")
+            // GetUsfm(usfm_to_txt).execute("https://cdn.door43.org/sr-Latn/stf/v1/mat.usfm")
+
+            val url = urlEditTv?.text.toString()
+
+            if (url.isNotEmpty()) {
+                if (Patterns.WEB_URL.matcher(url).matches()) {
+                    GetUsfm(usfm_to_txt).execute(url)
+                } else {
+                    Toast.makeText(this, "Invalid Url.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Url cannot be empty.", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> item.setOnMenuItemClickListener {
+                startActivity(Intent(this, MainActivity::class.java))
+                true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private inner class GetUsfm(etv: EditText) : AsyncTask<String, Unit, String>() {
@@ -75,6 +118,8 @@ class MainActivity : AppCompatActivity() {
             if (progressDialog!!.isShowing) {
                 progressDialog!!.dismiss()
             }
+            urlEditTv!!.visibility = View.GONE
+            btnView!!.visibility = View.GONE
         }
 
         private fun convertStreamToString(inputStream: BufferedInputStream): String {
